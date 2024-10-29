52d9b40c73efcaf3f9e97fd611d64709b28e8cce
ocfox <i@ocfox.me>
2024-10-29 10:41:47 +0800

    feat: use pnpm and flake packages
    close #10

--- flake.nix
+++ flake.nix
@@ -1,64 +1,77 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "";
-    };
-  };
-
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
-      git-hooks,
-      treefmt-nix,
+      flake-parts,
       ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      treefmtCfg = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
-    in
-    rec {
-      formatter.${system} = treefmtCfg.wrapper;
-      checks.${system} = {
-        formatting = treefmtCfg.check self;
-        git-hooks-check = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            deadnix = {
-              enable = true;
-              stages = [ "pre-push" ];
-            };
-            statix = {
-              enable = true;
-              stages = [ "pre-push" ];
-            };
-            nixfmt-rfc-style = {
-              enable = true;
-              stages = [
-                "pre-push"
-                "pre-commit"
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          lib,
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default =
+            let
+              inherit (pkgs) mkShell nodejs pnpm;
+            in
+            mkShell {
+              packages = [
+                nodejs
+                pnpm
               ];
             };
+
+          packages = rec {
+            nixpkgs-tracker = pkgs.stdenv.mkDerivation rec {
+              pname = "nixpkgs-tracker";
+              version =
+                let
+                  lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+                in
+                builtins.substring 0 8 lastModifiedDate;
+
+              src = ./.;
+
+              pnpmDeps = pkgs.pnpm.fetchDeps {
+                inherit pname version src;
+                hash = "sha256-zeSy2HwF0pBXmPRyjDkg2KpDE5sOU3auDLNk2vQ3sdk=";
+              };
+
+              nativeBuildInputs = with pkgs; [
+                nodejs
+                pnpm.configHook
+              ];
+
+              buildPhase = "pnpm build";
+
+              installPhase = ''
+                runHook preInstall
+
+                mkdir $out
+                mv dist $out
+
+                runHook postInstall
+              '';
+            };
+
+            default = nixpkgs-tracker;
           };
         };
-      };
-      devShells.${system}.default = pkgs.mkShellNoCC {
-        inherit (self.checks.${system}.git-hooks-check) shellHook;
-        buildInputs = self.checks.${system}.git-hooks-check.enabledPackages;
-        packages =
-          [ pkgs.yarn ]
-          ++ [
-            treefmtCfg.wrapper
-            (pkgs.lib.attrValues treefmtCfg.programs)
-          ];
-      };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
 }
